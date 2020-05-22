7f14353ac59ecf736a104860f9d5223830da04a9
OKO <ukusic@gmail.com>
2020-05-23 00:04:41 +0200

    mostly W0311

--- register-host.py
+++ register-host.py
@@ -5,10 +5,10 @@ import os
 import socket
 
 if os.geteuid() == 0:
-  print("Hey don't run script as root...")
-  exit()
+    print("Hey don't run script as root...")
+    exit()
 
-#if socket.gethostname() != "correct.server.example.com": 
+#if socket.gethostname() != "correct.server.example.com":
 #  print "Run script on correct server..."
 #  exit()
 
@@ -24,70 +24,70 @@ login_user = "fake_user"
 login_password_path = "/tmp/passfile"
 server = args["fqdn"]
 os_input = args["os"]
-vlan =  args["vlan"]
+vlan = args["vlan"]
 system = args["system"]
 inv_id = server[2:8]
 
 def print_variables():
-  print("--------------------")
-  print("Inv-id: {}".format(inv_id))
-  print("FQDN: {}".format(server))
-  print("OS: {}".format(os_value))
-  print("VLAN: {}".format(vlan))
-  print("System: {}".format(system))
-  print("--------------------")
- 
+    print("--------------------")
+    print("Inv-id: {}".format(inv_id))
+    print("FQDN: {}".format(server))
+    print("OS: {}".format(os_value))
+    print("VLAN: {}".format(vlan))
+    print("System: {}".format(system))
+    print("--------------------")
+
 def os_check():
-  global os_value
-  if os_input == "sles":
-    os_value = "SUSE Linux Enterprise Server"
-  elif os_input == "rhel":
-    os_value = "Red Hat Enterprise Linux"
-  else:
-    print("Abort script: Operating System not supported")
-    exit()
- 
+    global os_value
+    if os_input == "sles":
+      os_value = "SUSE Linux Enterprise Server"
+    elif os_input == "rhel":
+      os_value = "Red Hat Enterprise Linux"
+    else:
+      print("Abort script: Operating System not supported")
+      exit()
+
 def yes_or_no(question):
-  while "the answer is invalid":
-    reply = str(raw_input(question+' (y/n): ')).lower().strip()
-    if reply[:1] == 'y':
-      return True
-    if reply[:1] == 'n':
-      print("Abort script...")
-      exit()     
+    while "the answer is invalid":
+      reply = str(raw_input(question+' (y/n): ')).lower().strip()
+      if reply[:1] == 'y':
+        return True
+      if reply[:1] == 'n':
+        print("Abort script...")
+        exit()
 
 def login_cred():
-  if os.path.isfile(login_password_path):
-    login_pw = open(login_password_path,"r")
-    return(login_pw.read())
-  else:
-    print(login_password_path + " is missing..")
-    exit()
+    if os.path.isfile(login_password_path):
+      login_pw = open(login_password_path, "r")
+      return(login_pw.read())
+    else:
+      print(login_password_path + " is missing..")
+      exit()
 
 def payload(login_password):
-  content_input = {
-    "inv": inv_id,
-    "os": os_input,
-    "user": login_user,
-    "pass": login_password.rstrip()
-  }
-  data="""\
+    content_input = {
+      "inv": inv_id,
+      "os": os_input,
+      "user": login_user,
+      "pass": login_password.rstrip()
+    }
+    data= """\
 <invid>{inv}</invid>
 <operatingsystem>{os}</operatingsystem>
 <user>{user}</user>
 <pass>{pass}</pass>\
-  """
-  return(data.format(**content_input)) 
+    """
+    return(data.format(**content_input)) 
 
 def create_host(payload_output):
-  # make requests here
-  print(payload_output)
+    # make requests here
+    print(payload_output)
 
 def main():
-  os_check()
-  print_variables()
-  yes_or_no("Register host. Is above correct?")
-  create_host(payload(login_cred()))
+    os_check()
+    print_variables()
+    yes_or_no("Register host. Is above correct?")
+    create_host(payload(login_cred()))
 
-if __name__== "__main__":
-  main()
+if __name__ == "__main__":
+    main()
