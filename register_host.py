804d998db2be4cc6584fbbf62bca3ac98b88886b
OKO <ukusic@gmail.com>
2020-05-23 12:39:59 +0200

    C0103, C0111

--- register_host.py
+++ register_host.py
@@ -1,8 +1,5 @@
 #!/bin/python
 """register host"""
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
 import argparse
 import os
 import sys
@@ -15,40 +12,43 @@ if os.geteuid() == 0:
 #  print "Run script on correct server..."
 #  exit()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--fqdn", required=True, help="Host FQDN")
-parser.add_argument("-o", "--os", required=True, help="SLES or RHEL")
-parser.add_argument("-v", "--vlan", required=True, help="VLAN-ID")
-parser.add_argument("-s", "--system", required=True, help="System name")
-args = vars(parser.parse_args())
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-f", "--fqdn", required=True, help="Host FQDN")
+PARSER.add_argument("-o", "--os", required=True, help="SLES or RHEL")
+PARSER.add_argument("-v", "--vlan", required=True, help="VLAN-ID")
+PARSER.add_argument("-s", "--system", required=True, help="System name")
+ARGS = vars(PARSER.parse_args())
 
 # set some
 LOGIN_USER = "fake_user"
 LOGIN_PASSWORD_PATH = "/tmp/passfile"
-server = args["fqdn"]
-os_input = args["os"]
-vlan = args["vlan"]
-system = args["system"]
-inv_id = server[2:8]
+SERVER = ARGS["fqdn"]
+OS_INPUT = ARGS["os"]
+VLAN = ARGS["vlan"]
+SYSTEM = ARGS["system"]
+INV_ID = SERVER[2:8]
 
 def os_check():
-    if os_input == "sles":
+    """return os version"""
+    if OS_INPUT == "sles":
         return "SUSE Linux Enterprise Server"
-    if os_input == "rhel":
+    if OS_INPUT == "rhel":
         return "Red Hat Enterprise Linux"
     print("Abort script: Operating System not supported")
     sys.exit()
 
 def print_variables():
+    """print information"""
     print("--------------------")
-    print("Inv-id: {}".format(inv_id))
-    print("FQDN: {}".format(server))
+    print("Inv-id: {}".format(INV_ID))
+    print("FQDN: {}".format(SERVER))
     print("OS: {}".format(os_check()))
-    print("VLAN: {}".format(vlan))
-    print("System: {}".format(system))
+    print("VLAN: {}".format(VLAN))
+    print("System: {}".format(SYSTEM))
     print("--------------------")
 
 def yes_or_no(question):
+    """return true or exit"""
     while "the answer is invalid":
         reply = str(input(question+' (y/n): ')).lower().strip()
         if reply[:1] == 'y':
@@ -58,6 +58,7 @@ def yes_or_no(question):
             sys.exit()
 
 def login_cred():
+    """return password"""
     if os.path.isfile(LOGIN_PASSWORD_PATH):
         login_pw = open(LOGIN_PASSWORD_PATH, "r")
         return login_pw.read()
@@ -65,9 +66,10 @@ def login_cred():
     sys.exit()
 
 def payload(login_password):
+    """return payload"""
     content_input = {
-        "inv": inv_id,
-        "os": os_input,
+        "inv": INV_ID,
+        "os": OS_INPUT,
         "user": LOGIN_USER,
         "pass": login_password.rstrip()
     }
@@ -80,6 +82,7 @@ def payload(login_password):
     return data.format(**content_input)
 
 def create_host(payload_output):
+    """create host"""
     # make requests here
     print(payload_output)
 
